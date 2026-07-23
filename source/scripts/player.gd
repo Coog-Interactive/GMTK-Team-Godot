6357b7585920d43c254621c4fd4b275f9ab5eda8
jenocres <jenocres@tuta.com>
2026-07-22 20:36:01 -0500

    Tweak Sprinting, Add Momentum and Acceleration

--- source/scripts/player.gd
+++ source/scripts/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
 const WALK_SPEED: float = 300.0
-const DASH_SPEED: float = 500.0
+const ACCELERATION: float = 12.0
+const MIN_DASH_SPEED: float = 500.0
+const MAX_DASH_SPEED: float = 1000.0
 const JUMP_VELOCITY: float = -400.0
+
+const WALK_ANIM_SPEED_SCALE: float = 3.0
+const RUN_ANIM_SPEED_SCALE: float = 5.0
 
 var speed: float = 0.0
 var crouching: bool = false
@@ -11,17 +16,19 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	# Handle dash.
+	# Increasing dash.
 	if Input.is_action_pressed("dash") and is_on_floor():
-		speed = DASH_SPEED
+		speed += ACCELERATION
+		speed = clampf(speed, MIN_DASH_SPEED, MAX_DASH_SPEED)
 	elif is_on_floor():
-		speed = WALK_SPEED
-
+		speed -= ACCELERATION
+		speed = clampf(speed, WALK_SPEED, MAX_DASH_SPEED)
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: float = Input.get_axis("ui_left", "ui_right")
@@ -40,11 +47,10 @@ func _physics_process(delta: float) -> void:
 		$Sprite2D.flip_h = false
 	elif velocity.x < 0:
 		$Sprite2D.flip_h = true
-	
-	# TODO: Remove magic numbers
-	if speed == DASH_SPEED:
-		$Sprite2D.speed_scale = 5.0
+	 
+	if speed >= MIN_DASH_SPEED:
+		$Sprite2D.speed_scale = RUN_ANIM_SPEED_SCALE
 	else:
-		$Sprite2D.speed_scale = 3.0
+		$Sprite2D.speed_scale = WALK_ANIM_SPEED_SCALE
 
 	move_and_slide()
